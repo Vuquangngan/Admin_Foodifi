67851d8c82423c09fbecab121b2006b473935fea
Ngan <vuquangngan312@gmail.com>
2026-06-19 11:23:55 +0700

    update9

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-const ADMIN_ASSET_VERSION = "20260619-profile3";
+const ADMIN_ASSET_VERSION = "20260619-profile4";
 
 const { loadPartials } = await import(`./js/partials-loader.js?v=${ADMIN_ASSET_VERSION}`);
 
