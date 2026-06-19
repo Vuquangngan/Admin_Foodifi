69e9167577cfe91a3d3f9cc2b37a355debdb45ee
Ngan <vuquangngan312@gmail.com>
2026-06-19 16:26:05 +0700

    up

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-const ADMIN_ASSET_VERSION = "20260619-price-2x";
+const ADMIN_ASSET_VERSION = "20260619-ingredient-wrap";
 
 const { loadPartials } = await import(`./js/partials-loader.js?v=${ADMIN_ASSET_VERSION}`);
 
